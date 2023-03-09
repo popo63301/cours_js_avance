2f30c6c2c77ce53df2a824b17363bf8b35951f0e
popo63301 <popo63301@gmail.com>
2023-03-09 11:42:56 +0100

    notes challenges

---
+++ retour_challenges/notes.js
@@ -0,0 +1,73 @@
+const tab = [1, 2, 3];
+
+// à chaque élément, je veux appliquer la même transformation
+// mapper = correspondre
+// map = appliquer la même transformation à chaque élément
+
+const transfo1 = tab.map((e) => e * 2); //one-liner
+
+const transfo = tab.map((e) => {
+  //   console.log("e", e);
+  return e * 2;
+});
+
+//-----------
+// ajouter 5 à chaque élément de [1,2,3]
+
+//--------------------------
+
+const tab2 = [4, 7, 11];
+
+// reduce =
+// 1) parcour un tableau
+// 2) accumule les valeurs progressivement
+// 3) retourne 1 et 1 seule donnée
+
+const valueInit = 0; // valeur initial pour l'accumulateur
+
+const sum = tab2.reduce((accumulator, currentValue) => {
+  // console.log("acc", accumulator);
+  // console.log("curr", currentValue);
+  // console.log("sum des 2", accumulator + currentValue);
+  // console.log("----------------------");
+  return accumulator + currentValue;
+}, 0);
+
+// console.log(sum);
+
+//--------------------------
+
+const tabLettres = ["s", "a", "L", "u", "T"];
+
+const mot = tabLettres.reduce((acc, e) => {
+  if (e === e.toUpperCase()) {
+    return acc;
+  } else {
+    return acc + e;
+  }
+}, "");
+
+//--------------------------
+
+//copie d'objet + ajout de clé
+
+// const obj = { name: "sofiane", age: 26 };
+// const newObj = { ...obj, city: "Toulouse" }; // créer une copie + ajouter un nouvel élément
+
+// console.log(newObj);
+
+//--------------------------
+
+const names = ["sofiane", "jules", "toto"];
+
+const countLettersName = names.reduce((acc, e) => {
+  console.log("acc", acc);
+  console.log("e", e);
+  console.log("res", { ...acc, [e]: e.length });
+  console.log("----------------");
+  return { ...acc, [e]: e.length };
+}, {});
+
+// console.log(countLettersName);
+
+//--------------------------
---
+++ retour_challenges/sujet_1.js
@@ -0,0 +1,25 @@
+// const populations = [
+//   {
+//     id: 0,
+//     name: "Alan",
+//     jobs: ["dev junior", "dev fullstack"],
+//     password: "tyeedsa00",
+//   },
+//   { id: 1, name: "Albert", jobs: ["doctor"], password: "tyeidii00" },
+//   {
+//     id: 2,
+//     name: "Jhon",
+//     jobs: ["mathematician", "doctor"],
+//     password: "xyuuuoi00",
+//   },
+//   { id: 3, name: "Brice", jobs: ["dev fullstack"], password: "xytoiab00" },
+//   { id: 4, name: "Alexendra", jobs: ["dentist"], password: "aaaoiab33" },
+//   { id: 5, name: "Brad" },
+//   { id: 6, name: "Carl", jobs: ["lead dev", "dev fullstack"] },
+//   { id: 7, name: "Dallas", jobs: ["dev fullstack"] },
+//   { id: 8, name: "Dennis", jobs: ["integrator", "dev fullstack"] },
+//   { id: 9, name: "Edgar", jobs: ["mathematician"] },
+//   { id: 10, name: "Erika", jobs: ["computer scientist", "mathematician"] },
+//   { id: 11, name: "Isaac", jobs: ["doctor"], password: "Axgkj22Kl" },
+//   { id: 12, name: "Ian", password: "Axgkj00Kl" },
+// ];
